3c77ade704fde99c2655ef2eaddb182aae2ca672
Daria Doronina <skidle.dd@gmail.com>
2026-07-19 16:49:22 +0200

    Add: concatenation-of-array - submission-0

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int[] GetConcatenation(int[] nums) {
+        int numsLength = nums.Length;
+
+        int[] ans = new int[numsLength * 2];
+
+        int j = nums.Length;
+
+        for (int i = 0; i < numsLength - 1; i++)
+        {
+            ans[i] = nums[i];
+            ans[j] = nums[i];
+            j++;
+        }
+
+        return ans;
+    }
+}
