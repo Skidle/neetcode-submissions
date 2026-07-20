647eeeb913ec2d7e9a3f1d434175890a5a915369
Daria Doronina <skidle.dd@gmail.com>
2026-07-20 03:17:56 +0200

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.cs
@@ -0,0 +1,44 @@
+public class Solution {
+    public bool IsValid(string s) {
+        Stack<char> brackets = new Stack<char>();
+
+        foreach (char bracket in s) {
+            if (bracket == '(' || bracket == '{' || bracket == '[') {
+                brackets.Push(bracket);
+            } else {
+                if (brackets.Count == 0) {
+                    return false;
+                }
+                char prevBracket = brackets.Peek();
+
+                if (prevBracket == '(') {
+                    if (bracket == ')') {
+                        brackets.Pop();
+                    } else {
+                        return false;
+                    }
+                } else if (prevBracket == '{') {
+                    if (bracket == '}') {
+                        brackets.Pop();
+                    } else {
+                        return false;
+                    }
+                } else if (prevBracket == '[') {
+                    if (bracket == ']') {
+                        brackets.Pop();
+                    } else {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        if (brackets.Count == 0) {
+            return true;
+        }
+
+        return false;
+    }
+}
