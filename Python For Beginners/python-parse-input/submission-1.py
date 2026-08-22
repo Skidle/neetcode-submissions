a3d8b9824efec48f80e8ae277764bc69731af432
Daria Doronina <skidle.dd@gmail.com>
2026-08-22 23:16:37 +0200

    Add: python-parse-input - submission-1

---
+++ Python For Beginners/python-parse-input/submission-1.py
@@ -0,0 +1,13 @@
+from typing import List
+
+def read_integers() -> List[int]:
+    numbers = input().split(",")
+    result = []
+    for num in numbers:
+        result.append(int(num))
+    return result
+
+# do not modify the code below
+print(read_integers())
+print(read_integers())
+print(read_integers())
