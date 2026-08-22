be361a56c128e18e0a0e4a87c83ce7d54a9de607
Daria Doronina <skidle.dd@gmail.com>
2026-08-22 12:38:19 +0200

    Add: python-variable-types - submission-2

---
+++ Python For Beginners/python-variable-types/submission-2.py
@@ -0,0 +1,11 @@
+integer_type = 5
+float_type = 4.2
+bool_type = False
+string_type = "Hello"
+list_type = [0, 1, 2]
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
