210f4d0bacf164463195554636b202ebd24a80ce
Daria Doronina <skidle.dd@gmail.com>
2026-08-22 12:39:20 +0200

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,19 @@
+variable = 5
+
+print(type(variable))
+
+variable = -2.3
+
+print(type(variable))
+
+variable = True
+
+print(type(variable))
+
+variable = "Heyyyy"
+
+print(type(variable))
+
+variable = ["fds", "resfha"]
+
+print(type(variable))
